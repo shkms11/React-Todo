e742aa101d3b8667c8adb0f4cf31683271c18fe8
shkms11 <48136141+shkms11@users.noreply.github.com>
2023-08-10 23:20:29 +0600

    Update App.js
    typo

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function FormTodo({ addTodo }) {
 function App() {
   const [todos, setTodos] = React.useState([
     {
-      text: "This is a sampe todo",
+      text: "This is a sample todo",
       isDone: false
     }
   ]);
